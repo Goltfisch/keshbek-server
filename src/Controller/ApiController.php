ff8bb264eb4b80f4cd87bfbc3340289900e141b1
Alexander Wolf <alexander@goltfisch.de>
2019-03-23 23:32:11 +0100

    Add demo data and delete method for transactions

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -125,42 +125,4 @@ class ApiController extends AbstractController
         $request->request->replace($data);
         return $request;
     }
-    
-    /**
-     * Attempt authorization using jwt-verifier
-     *
-     * @return bool
-     */
-    public function isAuthorized(): bool
-    {
-        if (! isset($_SERVER['HTTP_AUTHORIZATION'])) {
-            return false;
-        }
-        $authType = null;
-        $authData = null;
-        // Extract the auth type and the data from the Authorization header.
-        @list($authType, $authData) = explode(" ", $_SERVER['HTTP_AUTHORIZATION'], 2);
-        // If the Authorization Header is not a bearer type, return a 401.
-        if ($authType != 'Bearer') {
-            return false;
-        }
-        // Attempt authorization with the provided token
-        try {
-            // Setup the JWT Verifier
-            $jwtVerifier = (new \Okta\JwtVerifier\JwtVerifierBuilder())
-                            ->setAdaptor(new \Okta\JwtVerifier\Adaptors\SpomkyLabsJose())
-                            ->setAudience('api://default')
-                            ->setClientId('0oaf2w64cvmQbcoUH0h7')
-                            ->setIssuer('https://dev-354685.oktapreview.com/oauth2/default')
-                            ->build();
-            // Verify the JWT from the Authorization Header.
-            $jwt = $jwtVerifier->verify($authData);
-        } catch (\Exception $e) {
-            // We encountered an error, return a 401.
-            return false;
-        }
-        return true;
-    }
-
-
 }
--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -12,6 +12,52 @@ use Symfony\Component\Routing\Annotation\Route;
 class TransactionController extends ApiController
 {
     /**
+     * @Route("/api/transaction/demo", methods="GET")
+     */
+    public function generateDemoData(Request $request, TransactionRepository $transactionRepository, EntityManagerInterface $em)
+    {        
+        $demoTransactions = [
+            [
+                'amount' => 100,
+                'reason' => 'Essen',
+            ],
+            [
+                'amount' => 200,
+                'reason' => 'Chillen',
+            ],
+            [
+                'amount' => 1337,
+                'reason' => 'Hacken',
+            ],
+            [
+                'amount' => 7,
+                'reason' => 'Zwiebeln',
+            ]
+        ];
+
+        $transactionDate = \DateTime::createFromFormat('d.m.Y', '23.03.2019');
+        $state = $em->getRepository('App\Entity\State')->findOneBy(['id' => 1]);
+        $creditor = $em->getRepository('App\Entity\User')->findOneBy(['id' => 1]);
+        $debitor = $em->getRepository('App\Entity\User')->findOneBy(['id' => 2]);
+
+        foreach($demoTransactions as $demoTransaction) {
+            $transaction = new Transaction();
+            $transaction->setCreditor($creditor);
+            $transaction->setDebitor($debitor);
+            $transaction->setAmount($demoTransaction['amount']);
+            $transaction->setReason($demoTransaction['reason']);
+            $transaction->setTransactionDate($transactionDate);
+            $transaction->setCreatedAt();
+            $transaction->setState($state);
+
+            $em->persist($transaction);
+            $em->flush();
+        }
+
+        return $this->respond([ 'response' => 'Demo data was created successfully.' ]);
+    }
+
+    /**
     * @Route("/api/transaction", methods="GET")
     */
     public function index(Request $request, TransactionRepository $transactionRepository)
@@ -24,7 +70,7 @@ class TransactionController extends ApiController
     }
 
     /**
-    * @Route("/api/transaction/{id}/", methods="GET")
+    * @Route("/api/transaction/{id}", methods="GET")
     */
     public function showTransaction(Request $request, TransactionRepository $transactionRepository)
     {
@@ -102,7 +148,7 @@ class TransactionController extends ApiController
     }
 
     /**
-     * @Route("/transaction/{id}", methods="DELETE")
+     * @Route("/api/transaction/{id}", methods="DELETE")
      */
     public function delete(Request $request, TransactionRepository $transactionRepository, EntityManagerInterface $em)
     {
@@ -112,7 +158,6 @@ class TransactionController extends ApiController
         $em->remove($transaction);
         $em->flush();
 
-        return 'user deleted';
+        return $this->respond([ 'response' => 'Transaction was deleted successfully.' ]);
     }
-
 }
